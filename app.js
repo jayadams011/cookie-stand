6eddac4156424f6eb1f18af03e3021775a7fb73c
jayadams011 <j.adams011@hotmail.com>
2017-12-11 15:50:40 -0800

    html further along. started on .js file

---
+++ app.js
@@ -0,0 +1,44 @@
+var firstAndPike = {
+  minCust: 23
+  maxCust: 65
+  avgCookSale:6.3
+    
+}
+
+
+
+
+
+
+/* var brian = {
+  age: 33,
+  prefession: 'instructor',
+  hobbies: ['skatebording', 'rap', 'code'],
+  details: function() {
+    return 'brian likes ' + this.hobbies[0] + ' and ' + this.hobbies[1];
+  }
+}
+
+function makeHTML() {
+  var container = document.createElement('div');
+  container.innerHTML = '<p>' + brian.details() + '</p>';
+  document.body.appendChild(container);
+
+
+  var list = document.createElement('ul');
+  var listArr = [];
+
+  for (var i = 0; i < brian.hobbies.length; i++) {
+    listArr.push('<li>' + brian.hobbies[i] + '</li>');
+  }
+
+  var fullList = listArr.join('');
+  list.innerHTML = fullList;
+  document.body.appendChild(list);
+ }
+
+makeHTML();*/
+
+
+
+//////
