4b5d477219ae638ca53394d47103897611748fdf
jayadams011 <j.adams011@hotmail.com>
2017-12-11 22:56:15 -0800

    moved javascript further along.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var hourlyList = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+var store = [firstAndPike, seaTac, seaCenter, capHill, alki];
 
 
 var firstAndPike = {
@@ -8,9 +9,20 @@ var firstAndPike = {
   maxCust: 65,
   avgCookSale: 6.3,
   avgCust: function(){
-    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+    return Math.round(Math.random()) * (this.maxCust - this.minCust) + this.minCust;
+  },
+
+  cookPerHour: function(){
+    var custAtHour = [];
+    for (var i = 0; i < hourlyList.length; i++) {
+      custAtHour.push(this.avgCust * this.avgCookSale);
+      return custAtHour;
+    }
+    avgCust();
+    cookPerHour();
   }
 };
+
 
 
 var seaTac = {
@@ -18,21 +30,37 @@ var seaTac = {
   maxCust: 24,
   avgCookSale: 1.2,
   avgCust: function(){
-    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
-
-  }
-
+    return Math.round(Math.random()) * (this.maxCust - this.minCust) + this.minCust;
+  },
+  cookPerHour: function(){
+    var custAtHour = [];
+    for (var i = 0; i < hourlyList.length; i++) {
+      custAtHour.push(this.avgCust * this.avgCookSale);
+      return custAtHour;
+    }
+    avgCust();
+    cookPerHour();
+  },
 };
+
 
 var seaCenter = {
   minCust: 11,
   maxCust: 38,
   avgCookSale: 3.7,
   avgCust: function(){
-    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+    return Math.round(Math.random()) * (this.maxCust - this.minCust) + this.minCust;
     console.log(avgCust);
+  },
+  cookPerHour: function(){
+    var custAtHour = [];
+    for (var i = 0; i < hourlyList.length; i++) {
+      custAtHour.push(this.avgCust * this.avgCookSale);
+      return custAtHour;
+    }
+    avgCust();
+    cookPerHour();
   }
-
 };
 
 
@@ -41,10 +69,17 @@ var capHill = {
   maxCust: 38,
   avgCookSale: 2.3,
   avgCust: function(){
-return Math.random() * (this.maxCust - this.minCust) + this.minCust;
-
+    return Math.round(Math.random()) * (this.maxCust - this.minCust) + this.minCust;
+  },
+  cookPerHour: function(){
+    var custAtHour = [];
+    for (var i = 0; i < hourlyList.length; i++) {
+      custAtHour.push(this.avgCust * this.avgCookSale);
+      return custAtHour;
+    }
+    avgCust();
+    cookPerHour();
   }
-
 };
 
 var alki = {
@@ -52,113 +87,45 @@ var alki = {
   maxCust: 16,
   avgCookSale: 4.6,
   avgCust: function(){
-    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
-  }
+    return Math.round(Math.random()) * (this.maxCust - this.minCust) + this.minCust;
+  },
   cookPerHour: function(){
     var custAtHour = [];
     for (var i = 0; i < hourlyList.length; i++) {
-      custAtHour.push(this.avgCust*this.avgCookSale);
-      return custAtHour
+      custAtHour.push(this.avgCust * this.avgCookSale);
+      return custAtHour;
     }
+    avgCust();
+    cookPerHour();
   }
-
-
 };
-
-
 
 function makeHTML() {
   var container = document.createElement('div');
   var listArr = [];
+  var list = document.createElement('ul');
+  var fullList = listArr.join('');
   for (var i = 0; i < hourlyList.length; i++) {
     listArr.push('<li>' + hourlyList[i] + store.cookPerHour[i] + '</li>');
-  container.innerHTML = '<p>' hourlyList[0] + store.avgCust() + '</p>';
-  document.body.appendChild(container);
-  var list = document.createElement('ul');
-
-
+    container.innerHTML = '<p>' + hourlyList[0] + store.avgCust() + '</p>';
+    document.body.appendChild(container);
+    list.innerHTML = fullList;
+    document.body.appendChild(list);
   }
 
-  var list = document.createElement('ul');
-  //   var listArr = [];
-  //
-  //   for (var i = 0; i < alki.hobbies.length; i++) {
-  //     listArr.push('<li>' + brian.hobbies[i] + '</li>');
-
-  var fullList = listArr.join('');
-  list.innerHTML = fullList;
-  document.body.appendChild(list);
-
-var printStoreCookHour(store) {
-
-  return store.avgCust;
-
+  //var printStore.cookPerHour(store) {
+  //return store.avgCust;
 }
-}
-
-printStoreMinCust(alki)
-
+//}
 makeHTML();
 
-// function getHourly(object) {
-//   for(var i = 0; i < hoursList.length; i++) {
-//     console.log([object].hourlySales());
-//     return [object].hourly.push([object].hourlySales());
-//   }
-// };
-//
-//
-//
-//
-//
-// // var brian = {
-// //   age: 33,
-// //   prefession: 'instructor',
-// //   hobbies: ['skatebording', 'rap', 'code'],
-// //   details: function() {
-// //     return 'brian likes ' + this.hobbies[0] + ' and ' + this.hobbies[1];
-// //   }
-// // }
-//
-// function makeHTML() {
-//   var container = document.createElement('div');
-//   container.innerHTML = '<p>' + alki.details() + '</p>';
-//   document.body.appendChild(container);
-//
-//
-//   var list = document.createElement('ul');
-//   var listArr = [];
-//
-//   for (var i = 0; i < alki.hobbies.length; i++) {
-//     listArr.push('<li>' + brian.hobbies[i] + '</li>');
-//   }
-//
-//   var fullList = listArr.join('');
-//   list.innerHTML = fullList;
-//   document.body.appendChild(list);
-//  }
-//
-// makeHTML();
-//
-//
-// //var alki = {
-// //   minCust: 2,
-// //   maxCust: 16,
-// //   avgCookSale: 4.6,
-// //   hourly: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
-// //   avgCustAlki: function(){
-// //     return Math.random() * (this.maxCust - this.minCust) + this.minCust;
-// //   }
-// // };
-// // avgCustAlki();
-//
-//
-//
-//
-//
-//
-// // Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
-// //
+
+
+
+
+
+
+
 // // Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
 // //
 // // Store the results for each location in a separate array... perhaps as a property of the object representing that location
