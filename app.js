094036fdb501938683f1e310a179f10ee94efaad
jayadams011 <j.adams011@hotmail.com>
2017-12-12 17:09:59 -0800

    deleted bad app.js and learning-journal.md. debugged app.js. modified some html.

--- app.js
+++ app.js
@@ -5,98 +5,74 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; // via MDN docs
 }
+var allStores = [];
 
-var pikePlaceMarket = {
-  name: '1st and Pike',
-  minCustsPerHour: 23,
-  maxCustsPerHour: 65,
-  avgCookiesPerCust: 6.3,
-  custsEachHour: [],
-  cookiesEachHour: [],
-  totalDailySales: 0,
-  calcCustsEachHour: function(){
+// Constructor Function
+function NewStores(name, minCustsPerHour, maxCustsPerHour, avgCookiesPerCust, custsEachHour,cookiesEachHour,totalDailySales){
+  this.name = name;
+  this.minCustsPerHour = minCustsPerHour;
+  this.maxCustsPerHour = maxCustsPerHour;
+  this.avgCookiesPerCust = avgCookiesPerCust;
+  this.custsEachHour = [];
+  this.cookiesEachHour = [];
+  this.totalDailySales = 0;
+  allStores.push(this);
+  this.calcCustsEachHour = function(){
     for(var i = 0; i < hours.length; i++){
       this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
     }
-  },
-  calcCookiesEachHour: function(){
+  };
+  this.calcCookiesEachHour = function(){
     this.calcCustsEachHour();
     for(var i = 0; i < hours.length; i++){
       var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
-      // console.log(oneHour, 'one hour');
       this.cookiesEachHour.push(oneHour);
       this.totalDailySales += oneHour;
-      // console.log(this.totalDailySales, 'total');
     }
-  },
-  render: function(){
+
+  };
+  this.render = function(){
     this.calcCookiesEachHour();
-    // access the element in the DOM where our stuff will go
-    var ulEl = document.getElementById('pike');
-    // console.log(ulEl, 'ulEl');
-    for(var i = 0; i < hours.length; i++){
 
-      // create an element
-      var liEl = document.createElement('li');
+  };
+}
 
-      // give it content
-      // 11am: 77 cookies
-      liEl.textContent = hours[i] + ': ' + this.cookiesEachHour[i] + ' cookies';
+    //  access the element in the DOM where our stuff will go
+      var ulEl = document.getElementById('id');
+      // console.log(ulEl, 'ulEl');
 
-      // append it to the parent
+        // create an element
+        var liEl = document.createElement('li');
+
+        // give it content
+        // 11am: 77 cookies
+        liEl.textContent = hours[i] + ': ' + this.cookiesEachHour[i] + ' cookies';
+
+        // append it to the parent
+        ulEl.appendChild(liEl);
+      //}
+      liEl = document.createElement('li');
+      liEl.textContent = 'Total: ' + this.totalDailySales + ' cookies';
       ulEl.appendChild(liEl);
-    }
-    liEl = document.createElement('li');
-    liEl.textContent = 'Total: ' + this.totalDailySales + ' cookies';
-    ulEl.appendChild(liEl);
-  }
-};
+},
+render: function(){
+      this.calcCookiesEachHour();
+}
 
-var seatacAirport = {
-  name: 'Seatac Airport',
-  minCustsPerHour: 3,
-  maxCustsPerHour: 24,
-  avgCookiesPerCust: 1.2,
-  custsEachHour: [],
-  cookiesEachHour: [],
-  totalDailySales: 0,
-  calcCustsEachHour: function(){
-    for(var i = 0; i < hours.length; i++){
-      this.custsEachHour.push(random(this.minCustsPerHour, this.maxCustsPerHour));
-    }
-  },
-  calcCookiesEachHour: function(){
-    this.calcCustsEachHour();
-    for(var i = 0; i < hours.length; i++){
-      var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
-      // console.log(oneHour, 'one hour');
-      this.cookiesEachHour.push(oneHour);
-      this.totalDailySales += oneHour;
-      // console.log(this.totalDailySales, 'total');
-    }
-  },
-  render: function(){
-    this.calcCookiesEachHour();
-    // access the element in the DOM where our stuff will go
-    var ulEl = document.getElementById('seatac');
-    // console.log(ulEl, 'ulEl');
-    for(var i = 0; i < hours.length; i++){
+  };
 
-      // create an element
-      var liEl = document.createElement('li');
 
-      // give it content
-      // 11am: 77 cookies
-      liEl.textContent = hours[i] + ': ' + this.cookiesEachHour[i] + ' cookies';
 
-      // append it to the parent
-      ulEl.appendChild(liEl);
-    }
-    liEl = document.createElement('li');
-    liEl.textContent = 'Total: ' + this.totalDailySales + ' cookies';
-    ulEl.appendChild(liEl);
-  }
-};
 
-pikePlaceMarket.render();
-seatacAirport.render();
+
+
+new NewStores('1st and Pike', 23, 65, 6.3);
+new NewStores('SeaTac Airport', 3, 24, 1.2);
+new NewStores('Seattle Center', 11, 38, 3.7);
+new NewStores('Capitol Hill', 20, 38, 2.3);
+new NewStores('Alki', 2, 16, 4.6);
+
+
+for(var i = 0; i < allStores.length; i++){
+  allStores[i].render();
+}
--- learning-journal.md
+++
@@ -1,3 +0,0 @@
-# Learning Journal
-
-This is my learning Journal. It is super dope. This is what the boss said to say for my first entry.
